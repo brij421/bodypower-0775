11491c3d3e7bdc7b1f34630cdd4e584af0ba7cdf
EC2 Default User <ec2-user@ip-172-31-6-139.eu-west-1.compute.internal>
2024-12-07 21:49:36 +0000

    full project

---
+++ members/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+class MembersConfig(AppConfig):
+    name = 'members'
+    default_auto_field = 'django.db.models.BigAutoField'  # Set default primary key field type to BigAutoField
