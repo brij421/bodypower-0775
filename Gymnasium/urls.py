9079bc4c325d152dedb52e785068072dfc47b5df
EC2 Default User <ec2-user@ip-172-31-6-139.eu-west-1.compute.internal>
2024-12-08 18:05:10 +0000

    change in buildspace file

--- Gymnasium/urls.py
+++ Gymnasium/urls.py
@@ -20,6 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
@@ -30,5 +31,7 @@ urlpatterns = [
     path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # for handling profile photos
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
