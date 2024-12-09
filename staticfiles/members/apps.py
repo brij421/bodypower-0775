aa485f57feb64e32ddf5779c65fef1a43557ad52
EC2 Default User <ec2-user@ip-172-31-6-139.eu-west-1.compute.internal>
2024-12-09 14:12:04 +0000

    Delete StaticFile

--- staticfiles/members/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class MembersConfig(AppConfig):
-    name = 'members'
-    default_auto_field = 'django.db.models.BigAutoField'  # Set default primary key field type to BigAutoField
--- staticfiles/reports/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-class ReportsConfig(AppConfig):
-    name = 'reports'
-    default_auto_field = 'django.db.models.BigAutoField'  # Set default primary key field type to BigAutoField
